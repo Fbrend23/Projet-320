554d29a2cc16c161527529875c34a59d2cbc5298
Fbrend23 <fbrend23@gmail.com>
2025-02-25 11:23:35 +0100

    Update projectile
    ajout attributs et propriétés

--- Projet 320/Projectile.cs
+++ Projet 320/Projectile.cs
@@ -8,13 +8,23 @@ namespace Projet_320
 {///
     internal class Projectile
     {
-         /// <summary>
-         /// Gère la trajectoire,la position et les collisions du projectile
-         /// </summary>
-     
+        /// <summary>
+        /// Gère la trajectoire,la position et les collisions du projectile
+        /// </summary>
 
+        public int _angle;
+        public int _power;
         //*********** Propriétés ***********//
         private string _projectile;
         private Position _position;
+
+        public int Angle
+        {
+            get { return _angle; }
+        }
+
+        public int Power
+        {
+            get { return _power; }
     }
 }
